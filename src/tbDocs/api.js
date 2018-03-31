e3b339a2cd9655a2d6388492655327579d45d36d
Frank Thürigen <frank_thuerigen@yahoo.de>
2018-03-31 07:25:00 +0200

    v8.0.38 added tb.debounce()

---
+++ src/tbDocs/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "tb",
+        "tb.CRUD",
+        "tb.Event",
+        "tb.Promise",
+        "tb.Require",
+        "tb.Selector",
+        "tb.Util",
+        "tb.dom"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
